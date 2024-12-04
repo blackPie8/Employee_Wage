5d0bf9e9e8009ad9fd7841f84a3f2bfa51a33e3b
blackPie8 <stanashray@gmail.com>
2024-12-04 14:00:33 +0530

    [Ashray] MOD : Daily Wages using switch

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Employee_Wage
 
 UC_1 -> Checks Employee Attendance using Math.random()fn
+
+-----------
+
+UC_2 -> Daily Wages using switch case
--- emp_wages.js
+++ emp_wages.js
@@ -1,14 +1,41 @@
+const partHour = 4
+const fullHour = 8
+const perHourWage = 20
+let totalWage = 0
+
 function randomVal(){
     const val = Math.floor(Math.random()*2)
     return val
 }
+
+function randomVal2(){
+    const val = Math.floor(Math.random()*3)
+    return val
+}
+
+function calcWage(n = randomVal2()){
+
+    switch(n){
+        case 0:
+        totalWage += partHour*perHourWage
+        break
+    
+        case 1:
+        totalWage += fullHour*perHourWage
+        break
+
+        case 2:
+        return
+        }
+    }
 
 let val = randomVal()
 
 if(val === 0){
     console.log("Absent")
 }
-
 else if(val === 1){
-    console.log("Present")        
+    console.log("Present")
+    calcWage()
+     console.log(`Wage for a day -> ${totalWage}`)           
 }
